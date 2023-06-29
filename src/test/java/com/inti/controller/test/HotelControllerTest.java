c1aa0580613e58b1455ff933f5840ced7b54a5b6
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-06-29 15:57:31 +0200

    Supression import inutile

--- src/test/java/com/inti/controller/test/HotelControllerTest.java
+++ src/test/java/com/inti/controller/test/HotelControllerTest.java
@@ -1,12 +1,10 @@
 package com.inti.controller.test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
