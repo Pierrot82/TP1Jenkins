82caddd0238be80abd2358578a2c4a17549295fe
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-06-29 11:24:28 +0200

    Tests HotelRepository

--- src/main/java/com/inti/model/Hotel.java
+++ src/main/java/com/inti/model/Hotel.java
@@ -1,5 +1,6 @@
 package com.inti.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class Hotel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idHotel;
 	@NonNull
+	@Column(length = 15)
 	private String nom;
 	@NonNull
 	private int nbEtoile;
--- src/test/java/com/inti/repository/test/HotelRepositoryTest.java
+++ src/test/java/com/inti/repository/test/HotelRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.inti.repository.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.inti.model.Hotel;
 import com.inti.repository.IHotelRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -13,6 +19,59 @@ public class HotelRepositoryTest {
 	
 	@Autowired
 	IHotelRepository ihr;
+	
+	Hotel h1;
+	
+	@BeforeEach
+	public void util() {
+		 h1 = new Hotel("IBIS Style", 3);
+	}
+	
+	@Test
+	public void saveHotelTest() {
+		//GIVEN
+
+		//WHEN
+		ihr.save(h1);
+		//THEN
+		assertThat(h1).isNotNull();
+		assertThat(h1.getNom()).isEqualTo("IBIS Style");
+	}
+	
+	@Test
+	public void saveHotelNullTest() {
+		//GIVEN
+		Hotel h2 = null;
+		//WHEN
+
+		//THEN
+		Assertions.assertThrows(Exception.class, () -> ihr.save(h2));
+	}
+	
+	@Test
+	public void saveHotelLengthNomSupperieurA15Test() {
+		
+		//GIVEN
+		Hotel h2 = new Hotel("fhgfjghjghkhkhjk", 2);
+		//WHEN
+
+		//THEN
+		Assertions.assertThrows(Exception.class, () -> ihr.save(h2));
+		
+	}
+	
+	@Test
+	public void deleteHotelTest() {
+		
+		//GIVEN
+
+		//WHEN
+		ihr.delete(h1);
+
+		//THEN
+		assertThat(h1).isNull();
+		
+	}
 	
 	
 
